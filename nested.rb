39278668e00c40e3a0d49e1687df2693713a74b8
Rei M <r.clarkemitchell@gmail.com>
2019-09-07 20:07:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,7 +88,7 @@ def adding_matz
 	 programmer_hash[:yukihiro_matsumoto][:languages] = []
 	 programmer_hash[:yukihiro_matsumoto][:languages] << "LISP"
 	 programmer_hash[:yukihiro_matsumoto][:languages] << "C"
-	 programmer_hash[:yukihiro_matsumoto]
+	 programmer_hash[:yukihiro_matsumoto][:known_for]
 end
 
 def changing_alan
